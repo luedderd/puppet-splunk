7aa54404d8446b312d47aa4c4450fda127002922
luedderd <74256035+luedderd@users.noreply.github.com>
2024-11-11 13:18:20 -0700

    cleanup

--- manifests/edgeprocessor/install.pp
+++ manifests/edgeprocessor/install.pp
@@ -3,19 +3,10 @@
 #   for successfully installing the Splunk Edge Processor
 #
 class splunk::edgeprocessor::install (
-  #String[1] $archive_name = regsubst($splunk::edgeprocessor::package_url, '([^\\/]+\\.tar\\.gz)$', '\\1'),
   String[1] $archive_name = regsubst($splunk::edgeprocessor::package_url, '(\\S*\\/)([^\\/]+\\.tar\\.gz$)', '\\2'),
   Stdlib::UnixPath $extract_path = regsubst($splunk::edgeprocessor::splunk_homedir, '(^.*\\/)[^\\/]*.$','\\1'),
-  #String[1] $archive_name = $splunk::edgeprocessor::package_url.match(),
-  #String[1] $archive_name = match($splunk::edgeprocessor::package_url, '([^\/]+\.tar\.gz)$')
 ) {
   #Download and unpack the Splunk Edge Processor
-  notify { 'homedir':
-    message => "variable homedir is: ${splunk::edgeprocessor::splunk_homedir}|",
-  }
-  notify { 'extract_path':
-    message => "variable extract_path is: ${extract_path}|",
-  }
   archive { $archive_name:
     path          => "/tmp/${archive_name}",
     source        => $splunk::edgeprocessor::package_url,
@@ -25,8 +16,6 @@ class splunk::edgeprocessor::install (
     extract_path  => $extract_path,
     creates       => $splunk::edgeprocessor::splunk_homedir,
     cleanup       => true,
-    # user          => $splunk::edgeprocessor::splunk_user,
-    # group         => $splunk::edgeprocessor::splunk_user,
   }
   file { $splunk::edgeprocessor::splunk_homedir:
     ensure  => directory,
