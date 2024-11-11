706ebbde1a16ba2ea5b83695a76f7fb869555c55
luedderd <74256035+luedderd@users.noreply.github.com>
2024-11-11 12:11:41 -0700

    fix title

--- manifests/edgeprocessor/install.pp
+++ manifests/edgeprocessor/install.pp
@@ -15,7 +15,7 @@ class splunk::edgeprocessor::install (
   notify { 'archive_name':
     message => "variable archive_name is: ${archive_name}|",
   }
-  archive { $splunk::edgeprocessor::archive_name:
+  archive { $archive_name:
     path          => "/tmp/${archive_name}",
     source        => $splunk::edgeprocessor::package_url,
     checksum      => $splunk::edgeprocessor::splunk_package_checksum,
